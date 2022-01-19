0f8d076ab005dd5d14f3fc39b0a74ee1f77d19a5
cmaspi <chiragmehta1010@gmail.com>
2022-01-19 21:11:40 +0530

    made more generic

--- sorting/__init__.py
+++ sorting/__init__.py
@@ -1,15 +1,18 @@
 # import statements
 import numpy as np
 import selectionSort as ss
+import bubbleSort as bs
 
 class sorting:
     def __init__(self) -> None:
         self.unsorted = np.array([3,2,5,1,6,4])
         
-    def selectionSort(self,arr: np.array=None):
-        if arr is None:
-            arr = self.unsorted
-        return ss.selectionsort(arr)
+    def selectionSort(self,arr: np.array, cmpfunc=None):
+        return ss.selectionsort(arr, cmpfunc)
+
+    def bubbleSort( self, arr: np.array, cmpfunc=None):        
+        return bs.bubblesort(arr, cmpfunc)
 
 sort = sorting()
-print(sort.selectionSort())
+print(sort.selectionSort(sort.unsorted))
+print(sort.bubbleSort(sort.unsorted))
--- sorting/selectionSort.py
+++ sorting/selectionSort.py
@@ -1,12 +1,16 @@
 # iterate through the array, find the minimum element, swap it with the first element. Now consider the array to be subarray of original array
 
-def selectionsort(array):
+def selectionsort(array, cmpfunc=None):
+    if cmpfunc is None:
+        cmpfunc = lambda x,y: x<y
+    elif cmpfunc == 'desc':
+        cmpfunc = lambda x,y: x>y
     arr = array.copy()
     n = len(arr)
     for i in range(n-1):
         min,minI = arr[i] , i
         for j in range(i+1,n):
-            if arr[j]<min:
+            if cmpfunc(arr[j],min):
                 min = arr[j]
                 minI = j
         temp = min
